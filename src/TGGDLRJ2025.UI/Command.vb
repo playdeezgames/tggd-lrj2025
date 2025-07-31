36b8fe718afa4b293c8c26cb418d69e1382a3734
TheGrumpyGameDev <ernestpazera@gmail.com>
2025-07-31 05:24:30 -0500

    works in the div

---
+++ src/TGGDLRJ2025.UI/Command.vb
@@ -0,0 +1,10 @@
+﻿Public Module Command
+    Public ReadOnly UP As String = NameOf(UP)
+    Public ReadOnly DOWN As String = NameOf(DOWN)
+    Public ReadOnly LEFT As String = NameOf(LEFT)
+    Public ReadOnly RIGHT As String = NameOf(RIGHT)
+    Public ReadOnly GREEN As String = NameOf(GREEN)
+    Public ReadOnly RED As String = NameOf(RED)
+    Public ReadOnly BLUE As String = NameOf(BLUE)
+    Public ReadOnly YELLOW As String = NameOf(YELLOW)
+End Module
